2ddae7677fcc536f6fb2536b50a0014df6cebacc
Alexander Vanspauwen <>
2022-08-24 22:44:33 +0200

    Further optimized handleBrie

--- js-jasmine/src/gilded_rose.js
+++ js-jasmine/src/gilded_rose.js
@@ -33,25 +33,23 @@ class Shop {
     }
   }
 
+  /** 
+   * Handles special case of item with name: {@link Names.BACKSTAGE}
+   */
   handleBackstage(item) {
     if (item.quality < 50) {
       item.quality = item.quality + 1;
-      if (item.name == 'Backstage passes to a TAFKAL80ETC concert') {
-        if (item.sellIn < 11) {
-          if (item.quality < 50) {
-            item.quality = item.quality + 1;
-          }
-        }
-        if (item.sellIn < 6) {
-          if (item.quality < 50) {
-            item.quality = item.quality + 1;
-          }
-        }
+      if (item.sellIn < 11 && item.quality < 50) {
+        item.quality = item.quality + 1;
+      }
+
+      if (item.sellIn < 6 && item.quality < 50) {
+        item.quality = item.quality + 1;
       }
     }
-    
+
     item.sellIn = item.sellIn - 1;
-    
+
     if (item.sellIn < 0) {
       item.quality = item.quality - item.quality;
     }
