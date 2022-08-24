c91926fc905ca90f71c213e01c873ab5a436d52f
Alexander Vanspauwen <>
2022-08-24 23:37:45 +0200

    Simplified handleBrie further

--- js-jasmine/src/gilded_rose.js
+++ js-jasmine/src/gilded_rose.js
@@ -31,15 +31,8 @@ class Shop {
    * Handles special case of item with name: {@link Names.BRIE}
    */
   handleBrie(item) {
-    if (item.quality < 50) {
-      this.adjustQuality(item, 1);
-    }
-
     item.sellIn = item.sellIn - 1;
-
-    if (item.sellIn < 0 && item.quality < 50) {
-      this.adjustQuality(item, 1);
-    }
+    this.adjustQuality(item, item.sellIn < 0 ? 2 : 1);
   }
 
   /** 
